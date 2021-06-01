7e7acd37e06abb1cc06871fb7ee95e0e6395c690
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-06-01 12:23:32 -0600

    Adds Onboarding Section

---
+++ components/Home/InstaFeed/InstaFeed.js
@@ -0,0 +1,9 @@
+import styles from './InstaFeed.module.scss'
+
+export default function InstaFeed() {
+    return (
+        <div className={styles.container}>
+            INSTAGRAM FEED
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Navbar from '../components/navbar/Navbar'
 
 import Banner from '../components/Home/Banner/Banner'
 import HowItWorks from '../components/Home/HowItWorks/HowItWorks'
+import Bestsellers from '../components/Home/Bestsellers/Bestsellers'
+import Onboarding from '../components/Home/Onboarding/Onboarding'
+import InstaFeed from '../components/Home/InstaFeed/InstaFeed'
 
 export default function Home() {
   return (
@@ -13,13 +16,11 @@ export default function Home() {
 
       <HowItWorks/>
 
-      <div>
-      <h2>SECCIÓN MÁS VENDIDOS</h2>
-      </div>
+      <Bestsellers/>
 
-      <div>
-      <h2>SECCIÓN ONBOARDING</h2>
-      </div>
+      <Onboarding/>
+
+      <InstaFeed/>
 
     </div>
   )
