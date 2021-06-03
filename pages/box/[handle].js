de9f3b45813445883919aed71ee7762eef54bf9f
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-06-03 12:51:05 -0600

    Adds Out Of The Box page component

---
+++ pages/box/[handle].js
@@ -0,0 +1,7 @@
+import styles from '../../styles/single-product.module.scss'
+
+export default function SingleProduct(){
+    return(
+        <div className={styles.container}>SINGLE PRODUCT PAGE</div>
+    )
+}
--- pages/out-of-the-box.js
+++ pages/out-of-the-box.js
@@ -3,28 +3,32 @@ import styles from '../styles/box-to-go.module.scss'
 import Products from '../components/ProductsPage/Products/Products'
 import { client } from '../utils/shopify'
 
-export default function BoxToGo({collection}){
+export default function OutOfTheBox({collection}){
+
+
     return (
         <div className={styles.PageContainer}>
             <div className={styles.PageTitle}>
-                <h1>ALL BOXES</h1>
+                <h1>OUT OF THE BOX</h1>
             </div>
             <div className={styles.Products}>
                 <div className={styles.Filters}>
                     <h3>FILTROS</h3>
                 </div>
                 <div className={styles.ProductsDisplay}>
-                    <Products collection={collection} link="box"/>
+                    <Products collection={collection} link="out-of-the-box"/>
                 </div>
             </div>
         </div>
     )
 }
 
+
+
 export async function getServerSideProps() {
     // Fetch data from external API
 
-    const collectionId = 'Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzIxMjM4MTEzOTEwNg==';
+    const collectionId = 'Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzIzNjk4NjkyNTIxOA==';
 
     const collection = await client.collection.fetchWithProducts(collectionId, {productsFirst: 8})
     
