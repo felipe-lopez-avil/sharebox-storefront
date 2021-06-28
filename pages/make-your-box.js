380fcabc6edb3ddbb21089d124200b28cd6f4d35
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-06-28 11:58:24 -0600

    Removes Ant Design Stepper

--- pages/make-your-box.js
+++ pages/make-your-box.js
@@ -1,13 +1,13 @@
 import {useState} from 'react'
 import styles from '../styles/make-your-box.module.scss'
 
-import { Steps } from 'antd';
-import "antd/dist/antd.css";
+/* import { Steps } from 'antd';*/
+//import "antd/dist/antd.css"; 
 
 import MakeYourBoxSlider from '../components/SliderMYB/SliderMYB'
 import MultiStepForm from '../components/MakeYourBox/MultiStepForm/MultiStepForm'
 
-const { Step } = Steps;
+// const { Step } = Steps;
 
 export default function MakeYourBox() {
 
@@ -18,12 +18,12 @@ export default function MakeYourBox() {
             {/*Columna izquierda. Sección donde se muestran los pasos del proceso así 
             como los porductos que llevas en el proceso*/}
             <div className={styles.summary}>
-                <Steps direction="vertical" current={currentStep}>
+                {/* <Steps direction="vertical" current={currentStep}>
                     <Step title="PASO 1." description="Let the Magic Begin."/>
                     <Step title="PASO 2." description="Something Special." />
                     <Step title="PASO 3." description="Go Extra." />
                     <Step title="PASO 4." description="Share Happiness!."/>
-                </Steps>
+                </Steps> */}
             </div>
 
             {/*Columna derecha. Sección donde se muestran los productos a elegir*/}
