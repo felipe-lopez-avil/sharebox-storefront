492cfebe8afda655457387bdd9ac91ae77471343
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-10-12 09:27:05 -0500

    Milestone (Home Page): Best Sellers Section

--- components/HomePage/Bestsellers/Bestsellers.js
+++ components/HomePage/Bestsellers/Bestsellers.js
@@ -1,7 +1,129 @@
 import styles from './Bestsellers.module.scss'
+import Image from 'next/image'
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/core';
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/pagination/pagination.min.css"
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+const products = [
+    {
+        productTitle: "Test Product",
+        price: "400.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/snack-jar-1_3f71cb90-c647-476c-9e35-c98d9440da0a.jpg?v=1627671915",
+    },
+    {
+        productTitle: "Product Fake",
+        price: "200.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/shiro-y-tessa.jpg?v=1626987626",
+    },
+    {
+        productTitle: "Product Test",
+        price: "350.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/wine-box-for-her.jpg?v=1627420835",
+    },
+    {
+        productTitle: "Other Test",
+        price: "235.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/bff-box_1.jpg?v=1627419297",
+    },
+    {
+        productTitle: "Test Again",
+        price: "750.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/rose-box.jpg?v=1632430978",
+    },
+    {
+        productTitle: "Fake Product",
+        price: "650.00",
+        image: "https://cdn.shopify.com/s/files/1/0456/6820/4706/products/SKINCARE-box-3.jpg?v=1626978872",
+    },
+]
 
 export default function Bestsellers() {
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h3>Our Bests Sellers</h3>
+            </div>
+            <div className={styles.products}>
+                <Swiper 
+                    freeMode={true}
+                    slidesPerView={4}
+                    pagination={{
+                        "clickable": true
+                    }}
+                    autoplay={{
+                        "delay": 3000,
+                        "disableOnInteraction": false
+                    }}
+                >
+                    {products.map(product => (
+                        <SwiperSlide>
+                            <div className={styles.centerCard}>
+                                <div className={styles.productCard}>
+                                    <div className={styles.productImage}>
+                                        <Image
+                                            src={product.image}
+                                            layout="fill"
+                                            objectFit="cover"
+                                        />
+                                    </div>
+                                    <div className={styles.productTitle}>{product.productTitle}</div>
+                                    <div className={styles.productPrice}>${product.price}</div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                    {/* <SwiperSlide>
+                        <div className={styles.centerCard}>
+                            <div className={styles.productCard}>
+                                <div className={styles.productImage}></div>
+                                <div className={styles.productTitle}>Product Title</div>
+                                <div className={styles.productPrice}>$250.00</div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className={styles.centerCard}>
+                            <div className={styles.productCard}>
+                                <div className={styles.productImage}></div>
+                                <div className={styles.productTitle}>Product Title</div>
+                                <div className={styles.productPrice}>$250.00</div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className={styles.centerCard}>
+                            <div className={styles.productCard}>
+                                <div className={styles.productImage}></div>
+                                <div className={styles.productTitle}>Product Title</div>
+                                <div className={styles.productPrice}>$250.00</div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className={styles.centerCard}>
+                            <div className={styles.productCard}>
+                                <div className={styles.productImage}></div>
+                                <div className={styles.productTitle}>Product Title</div>
+                                <div className={styles.productPrice}>$250.00</div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className={styles.centerCard}>
+                            <div className={styles.productCard}>
+                                <div className={styles.productImage}></div>
+                                <div className={styles.productTitle}>Product Title</div>
+                                <div className={styles.productPrice}>$250.00</div>
+                            </div>
+                        </div>
+                    </SwiperSlide> */}
+                </Swiper>
+            </div>
+        </div>
     )
 }
--- components/HomePage/Onboarding/Onboarding.js
+++ components/HomePage/Onboarding/Onboarding.js
@@ -2,23 +2,23 @@ import styles from './Onboarding.module.scss'
 import Image from 'next/image'
 
 import {Swiper, SwiperSlide} from 'swiper/react';
-import SwiperCore, { Navigation, Autoplay } from 'swiper/core';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/core';
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/pagination/pagination.min.css"
 
-SwiperCore.use([Navigation, Autoplay]);
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default function Onboarding() {
     return(
         <div className={styles.container}>
             <Swiper 
-                navigation={true}
-                cssMode={true}
+                pagination={{
+                    "clickable": true
+                }}
                 slidesPerView={1}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
                 autoplay={{
-                    "delay": 5000,
+                    "delay": 50000,
                     "disableOnInteraction": false
                 }}
             >
@@ -57,7 +57,7 @@ export default function Onboarding() {
                         </div>
                         <div className={styles.image}>
                             <Image
-                                src="https://cdn.shopify.com/s/files/1/0456/6820/4706/files/Mano_Cel_2.png?v=1633966167"
+                                src="https://cdn.shopify.com/s/files/1/0456/6820/4706/files/Mano_Cel_3.png?v=1633987558"
                                 layout="fill"
                                 objectFit="contain"
                             />
