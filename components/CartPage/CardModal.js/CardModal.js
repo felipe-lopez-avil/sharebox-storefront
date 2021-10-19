808c2e1362fdbd609fef1b2af73fdb2e371d5c2b
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-10-19 15:36:25 -0500

    Responsive Modals

--- components/CartPage/CardModal.js/CardModal.js
+++ components/CartPage/CardModal.js/CardModal.js
@@ -47,38 +47,86 @@ export default function CardModal ({closeCardModal, selectedCard, setSelectedCar
                 </div>
             </div>
             <div className={`${styles.content} ${cardPreview ? styles.hidden : ''}`}>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Cumpleaños', 'https://cdn.shopify.com/s/files/1/0456/6820/4706/files/happy-birthday.png?v=1630507598')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('Birthday', '/cards/bdayCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/bdayIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>Birthday</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Aniversario', 'https://cdn.shopify.com/s/files/1/0456/6820/4706/files/aniversary-04.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('Anniversary', '/cards/anniversaryCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/aniversaryIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>Aniversary</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Felicitaciones','https://cdn.shopify.com/s/files/1/0456/6820/4706/files/congratulations-06.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('Congratulations','/cards/congratsCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/congratulationsIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>Congratulations</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Felicitaciones','https://cdn.shopify.com/s/files/1/0456/6820/4706/files/congratulations-06.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('To My Special Someone','https://cdn.shopify.com/s/files/1/0456/6820/4706/files/congratulations-06.png?v=1630517306')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/specialIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>To My Special Someone</div>
                 </div>
             </div>
             <div className={`${styles.content} ${cardPreview ? styles.hidden : ''}`}>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Cumpleaños', 'https://cdn.shopify.com/s/files/1/0456/6820/4706/files/happy-birthday.png?v=1630507598')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('She Said Yes', '/cards/brideCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/brideIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>She Said Yes</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Aniversario', 'https://cdn.shopify.com/s/files/1/0456/6820/4706/files/aniversary-04.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('Thank You', '/cards/thanksCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/graciasIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>Thank You</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Felicitaciones','https://cdn.shopify.com/s/files/1/0456/6820/4706/files/congratulations-06.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
-                    <div className={styles.cardType}>With deepest simpaty</div>
+                <div className={styles.cardBox} onClick={() => showPreview('With deepest simpathy','/cards/simpathyCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/condolencesIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
+                    <div className={styles.cardType}>With deepest simpathy</div>
                 </div>
-                <div className={styles.cardBox} onClick={() => showPreview('Tarjeta de Felicitaciones','https://cdn.shopify.com/s/files/1/0456/6820/4706/files/congratulations-06.png?v=1630517306')}>
-                    <div className={styles.iconContainer}></div>
+                <div className={styles.cardBox} onClick={() => showPreview('Happy Mothers Day','/cards/motherCard.png')}>
+                    <div className={styles.iconContainer}>
+                        <Image
+                            src="/cartModalIcons/motherIcon.png"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
                     <div className={styles.cardType}>Happy Mothers Day</div>
                 </div>
             </div>
@@ -92,22 +140,22 @@ export default function CardModal ({closeCardModal, selectedCard, setSelectedCar
                             <form noValidate autoComplete="off">
                                 <div className={styles.textField}>
                                     <TextField 
-                                        id="from" 
-                                        label="Quién envía" 
-                                        variant="outlined" 
-                                        fullWidth="true"
-                                        value={cardFrom}
-                                        onChange={handleFromChange}
-                                    />
-                                </div>
-                                <div className={styles.textField}>
-                                    <TextField 
                                         id="for" 
                                         label="A quién se envía" 
                                         variant="outlined" 
                                         fullWidth="true" 
                                         value={cardTo}
                                         onChange={handleToChange}
+                                    />
+                                </div>
+                                <div className={styles.textField}>
+                                    <TextField 
+                                        id="from" 
+                                        label="Quién envía" 
+                                        variant="outlined" 
+                                        fullWidth="true"
+                                        value={cardFrom}
+                                        onChange={handleFromChange}
                                     />
                                 </div>
                                 <div className={styles.textField}>
@@ -132,9 +180,9 @@ export default function CardModal ({closeCardModal, selectedCard, setSelectedCar
                                     objectFit="cover"
                                 />
                                 <div className={styles.textPreview}>
-                                    <div className={styles.from}>{cardFrom}</div>
-                                    <div className={styles.message}>{cardMessage}</div>
                                     <div className={styles.to}>{cardTo}</div>
+                                    <div className={styles.message}>{cardMessage}</div>
+                                    <div className={styles.from}>{cardFrom}</div>
                                 </div>
                             </div>
                             <div className={styles.save} onClick={saveCardAttributes}>
