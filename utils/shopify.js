bad32b2089fc268311735aea16b818b7c1969f83
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-10-20 02:26:45 -0500

    New client link

--- utils/shopify.js
+++ utils/shopify.js
@@ -2,7 +2,7 @@ import Client from 'shopify-buy';
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: 'sharebox.mx',
+  domain: 'share-box-mx.myshopify.com',
   storefrontAccessToken: 'bbc70a51f39a76e0312599533016f059'
 });
 
