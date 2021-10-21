45ecd0620e15069f4bbcfcc62ae30ee10d6f5efa
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2021-10-21 16:24:48 -0500

    Adds GA Tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag'
 import '../styles/globals.scss'
 import Head from 'next/head';
 import Layout from '../components/Layout/Layout'
@@ -11,12 +14,37 @@ config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatic
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
         <title>Sharebox</title>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
+      {/* <Script
+        strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=UA-177859708-1'}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-177859708-1');
+        `}
+      </Script> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>    
